9de2002982ffe506ec1ee20610343acb70ee53ee
WaxKinetic <waxkinetic@gmail.com>
2013-01-25 12:21:09 -0800

    setup.py metadata

--- setup.py
+++ setup.py
@@ -1,34 +1,25 @@
 from __future__ import absolute_import
 
-from collections import defaultdict
-from itertools import chain, imap, repeat
-from os import walk
-from os.path import join
 from setuptools import setup, find_packages
-import re
 
-
-def all_files_under(**kwargs):
-    result = defaultdict(list)
-    for package, start_paths in kwargs.iteritems():
-        if isinstance(start_paths, basestring):
-            start_paths = [start_paths]
-        for start_path in start_paths:
-            top = join(package, start_path)
-            prefix = re.compile('^' + re.escape(top))
-            result[package].extend(chain.from_iterable(imap(join, repeat(prefix.sub(start_path, dirpath)), filenames)
-                for dirpath, _, filenames in walk(top)))
-    return result
+readme = open('README.md').read()
 
 setup(
     name='fabcloudkit',
-    version='0.01',
-    description='An AWS provisioning, build, and deployment library built on Fabric and Boto.',
-    zip_safe=False,
+    version='0.01-dev',
+    url='http://github.com/waxkinetic/fabcloudkit',
+    license='BSD',
 
-    # only focusing on dependencies.
+    author='Rick Bohrer',
+    author_email='waxkinetic@gmail.com',
+
+    description='An AWS provisioning, build, and deployment library built on Fabric and Boto.',
+    long_description=readme,
+
+    zip_safe=False,
+    include_package_data=True,
+
     packages=find_packages(),
-#    package_data=all_files_under(app=['static', 'templates']),
 
     setup_requires=[
         'setuptools-git >= 1.0b1'
