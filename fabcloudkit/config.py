6aa3a43161b44672bfd7c1495cdc0582635c0f0e
WaxKinetic <waxkinetic@gmail.com>
2013-03-17 18:16:29 -0700

    restored names

--- fabcloudkit/config.py
+++ fabcloudkit/config.py
@@ -71,7 +71,7 @@ class Config(dotdict):
     def load_file(self, path=None):
         if not self._loaded:
             if path is None:
-                path = resource_filename(__name__, 'fabcloudkit.yaml.py')
+                path = resource_filename(__name__, 'fabcloudkit.yaml')
             with open(path, 'r') as f:
                 self._set_dct(yaml.safe_load(f.read()))
         self._loaded = True
